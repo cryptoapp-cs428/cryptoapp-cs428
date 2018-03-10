10b12744d865efbc184996ca91c9d9713a968508
Skyler Laptop <smithskylera@gmail.com>
2018-03-10 11:47:13 -0700

    Added API for backend to listen to solidity events.

--- solidity/facades/backend.js
+++ solidity/facades/backend.js
@@ -1,13 +1,83 @@
+const { address } = require('../deployed_main_contract.json');
+const mainABI = require('./build_abis/Main_abi.json');
 const mockData = require('./_mockData.json');
 const Shape = require('./shape');
 
 var web3 = null;
+var mainContract;
+var eventShapeAdded, eventChallengePosted, eventChallengeResolved, eventChallengeRejected, eventRandomPosted, eventRandomResolved;
 function useWeb3(newWeb3) {
 	Shape.useWeb3(newWeb3);
 	web3 = newWeb3;
+	var MainContract = web3.eth.contract(abi);
+	mainContract = MainContract.at(address);
+	getEvents();
+}
+
+function getEvents() {
+	eventShapeAdded = mainContract.ShapeAdded({}, {fromBlock: 0, toBlock: 'latest'});
+	eventChallengePosted = mainContract.ChallengePosted({}, {fromBlock: 0, toBlock: 'latest'});
+	eventChallengeResolved = mainContract.ChallengeResolved({}, {fromBlock: 0, toBlock: 'latest'});
+	eventChallengeRejected = mainContract.ChallengeRejected({}, {fromBlock: 0, toBlock: 'latest'});
+	eventRandomPosted = mainContract.RandomPosted({}, {fromBlock: 0, toBlock: 'latest'});
+	eventRandomResolved = mainContract.RandomResolved({}, {fromBlock: 0, toBlock: 'latest'});
 }
 
 const shapes = mockData.shapes.map(Shape.fromJSON);
+
+/* on(eventKey, callback) Example usage:
+	solidityAPI.on("shapeAdded", function(shapeAddress, ownerAddress) {
+		// Called by the API whenever a shape is added.
+	})
+*/
+function on(eventKey, callback) {
+	switch eventKey {
+		case "shapeAdded":
+			eventShapeAdded.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.shapeAddress, result.args.owner);
+				}
+			});
+			break;
+		case "challengePosted":
+			eventChallengePosted.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.sourceShape, result.args.targetShape);
+				}
+			});
+			break;
+		case "challengeResolved":
+			eventChallengeResolved.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.sourceShape, result.args.targetShape, result.args.sourceWon);
+				}
+			});
+			break;
+		case "challengeRejected":
+			eventChallengeRejected.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.sourceShape, result.args.targetShape);
+				}
+			});
+			break;
+		case "randomPosted":
+			eventRandomPosted.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.shapeAddress);
+				}
+			});
+			break;
+		case "randomResolved":
+			eventRandomResolved.watch(function(error, result) {
+				if (!error) {
+					callback(result.args.winnerShapeAddress, result.args.loserShapeAddress);
+				}
+			});
+			break;
+		default:
+			break;
+	}
+}
 
 module.exports = {
 	// Include Shape class for reference
@@ -19,5 +89,6 @@ module.exports = {
 	async resolveRandomMatch(shapeID1, shapeID2) {
 		// Stub function, does nothing yet
 	},
+	on,
 	useWeb3,
 };
