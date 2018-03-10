53f1a17755fecbac5f953d82ad4f9159b3ce551b
Dan Mercer <danielmercer1@gmail.com>
2018-03-09 17:12:27 -0700

    More work on backend facade

--- solidity/facades/backend.js
+++ solidity/facades/backend.js
@@ -1,15 +1,23 @@
 const mockData = require('./_mockData.json');
 const Shape = require('./shape');
 
-// TODO: call Shape.useWeb3(?) with some web3 instance
+var web3 = null;
+function useWeb3(newWeb3) {
+	Shape.useWeb3(newWeb3);
+	web3 = newWeb3;
+}
 
 const shapes = mockData.shapes.map(Shape.fromJSON);
 
 module.exports = {
+	// Include Shape class for reference
+	Shape,
+
 	async getAllShapes(){
 		return shapes;
 	},
 	async resolveRandomMatch(shapeID1, shapeID2) {
 		// Stub function, does nothing yet
 	},
+	useWeb3,
 };
--- solidity/tests/backend.test.js
+++ solidity/tests/backend.test.js
@@ -1,11 +1,23 @@
 const assert = require('assert');
 const backendFacade = require('../facades/backend');
 
+// backendFacade.useWeb3(require('../web3/ganache'));
+
 describe("Backend facade", () => {
-	it("has a getAllShapes function", () => {
-		assert.equal(typeof backendFacade.getAllShapes, 'function');
-	});
 	it("has a resolveRandomMatch function", () => {
 		assert.equal(typeof backendFacade.getAllShapes, 'function');
+	});
+	describe("getAllShapes", () => {
+		it("is a function", () => {
+			assert.equal(typeof backendFacade.getAllShapes, 'function');
+		});
+		it("returns a Promise for an array of Shapes", (done) => {
+			backendFacade.getAllShapes().then(shapes => {
+				assert(shapes instanceof Array);
+				shapes.forEach(shape => {
+					assert(shape instanceof backendFacade.Shape);
+				});
+			}).then(done);
+		});
 	});
 });
