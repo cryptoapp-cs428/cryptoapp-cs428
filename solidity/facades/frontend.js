3a9af9a9b4e121e2466e55460e87a5b49f15125a
Dan Mercer <danielmercer1@gmail.com>
2018-03-12 16:35:53 -0600

    Translate frontend.js to ES5 to fix build

--- solidity/facades/frontend.js
+++ solidity/facades/frontend.js
@@ -1,20 +1,32 @@
-const { address } = require('../deployed_main_contract.json');
+/*
+ * This file must use only ES5 syntax, because it's not run through Babel
+ * before being minified by Webpack.
+ */
+
+const address = require('../deployed_main_contract.json').address;
 const abi = require('./build_abis/Main_abi.json');
 
 module.exports = function(web3) {
 
 	const mainContract = new web3.eth.Contract(abi, address);
 
-	// Returns a promise for the default account, or the first account if no default is set
-	const getUserAccount = () =>
-			web3.eth.defaultAccount
-				? Promise.resolve(web3.eth.defaultAccount)
-				:	web3.eth.getAccounts().then(accounts => accounts[0]);
-
 	return {
-		getUserAccount,
-		getAnimalCount: async () => 0,
-		animalIndexToOwner: async (index) => null,
-		getUserAnimals: async () => [],
+		// Returns a promise for the default account, or the first account if no default is set
+		getUserAccount: function() {
+			return web3.eth.defaultAccount
+				? Promise.resolve(web3.eth.defaultAccount)
+				:	web3.eth.getAccounts().then(function(accounts) {
+					return accounts[0];
+				})
+		},
+		getAnimalCount: function() {
+			return Promise.resolve(0);
+		},
+		animalIndexToOwner: function() {
+			return Promise.resolve(null);
+		},
+		getUserAnimals: function() {
+			return Promise.resolve([]);
+		},
 	};
 }
