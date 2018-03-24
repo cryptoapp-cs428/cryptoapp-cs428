0eb2662aca4d525217234dd8fde6e9560017b07c
Dan Mercer <danielmercer1@gmail.com>
2018-03-24 15:39:31 -0600

    Main contract: unit tests for getShapes()

--- solidity/tests/contracts.test.js
+++ solidity/tests/contracts.test.js
@@ -6,12 +6,18 @@ const Main = require('../build/CryptoShapeMain_full.json');
 const abi = Main['interface'];
 const bytecode = Main['bytecode'];
 
-let accts, contract, deployer;
+let accts,
+	contract,
+	deployer,
+	user1,
+	user2;
 
 beforeEach(async () => {
 		// Get list of accts
 		accts = await web3.eth.getAccounts();
 		deployer = accts[0];
+		user1 = accts[1];
+		user2 = accts[2];
 
 		// Use acct to deploy contract
 		contract = await new web3.eth.Contract(JSON.parse(abi))
@@ -26,6 +32,23 @@ beforeEach(async () => {
 		contract.setProvider(web3.currentProvider);
 });
 
+//======================================================================
+//			Utility functions
+
+function deployShapeFrom(acct) {
+	return contract.methods.buyShape().send({
+		from: acct,
+		value: web3.utils.toWei('0.01', 'ether'),
+		gas: '3000000'
+	});
+}
+
+const isAddress = web3.utils.isAddress;
+const allTruthy = arr => !!arr.reduce((a, b) => a && b);
+
+//======================================================================
+//			Test cases
+
 describe("Main contract", () => {
 	it("deploys properly", () => {
 		assert.ok(contract.options.address);
@@ -33,6 +56,33 @@ describe("Main contract", () => {
 
 	it("stores the deployer as the manager field", async () => {
 		const manager = await contract.methods.manager().call();
+
 		assert.equal(manager, deployer);
+	});
+
+	describe("getShapes()", () => {
+		it("returns an empty list of shapes (initially)", async () => {
+			const shapes = await contract.methods.getShapes().call();
+
+			assert.deepEqual(shapes, []);
+		});
+		it("returns a list of shape addresses", async () => {
+			await deployShapeFrom(user1);
+
+			const shapes = await contract.methods.getShapes().call();
+
+			assert.equal(shapes.length, 1);
+			assert(isAddress(shapes[0]));
+		});
+		it("can return multiple addresses", async () => {
+			await deployShapeFrom(user1);
+			await deployShapeFrom(user1);
+			await deployShapeFrom(user2);
+
+			const shapes = await contract.methods.getShapes().call();
+
+			assert.equal(shapes.length, 3);
+			assert(allTruthy(shapes.map(isAddress)));
+		});
 	});
 });
