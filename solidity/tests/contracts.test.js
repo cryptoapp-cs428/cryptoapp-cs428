1069b3c50e5baa0adebfbbdebe72a08a035ccbd7
Dan Mercer <danielmercer1@gmail.com>
2018-03-24 15:25:26 -0600

    Main contract: added unit test for manager()

--- solidity/tests/contracts.test.js
+++ solidity/tests/contracts.test.js
@@ -6,23 +6,22 @@ const Main = require('../build/CryptoShapeMain_full.json');
 const abi = Main['interface'];
 const bytecode = Main['bytecode'];
 
-let accts, contract, sendOpts;
+let accts, contract, deployer;
 
 beforeEach(async () => {
 		// Get list of accts
 		accts = await web3.eth.getAccounts();
-
-		sendOpts = {
-			from: accts[0],
-			gas: '3000000'
-		};
+		deployer = accts[0];
 
 		// Use acct to deploy contract
 		contract = await new web3.eth.Contract(JSON.parse(abi))
 			.deploy({
 				data: bytecode
 			})
-			.send(sendOpts)
+			.send({
+				from: deployer,
+				gas: '3000000'
+			})
 
 		contract.setProvider(web3.currentProvider);
 });
@@ -30,5 +29,10 @@ beforeEach(async () => {
 describe("Main contract", () => {
 	it("deploys properly", () => {
 		assert.ok(contract.options.address);
+	});
+
+	it("stores the deployer as the manager field", async () => {
+		const manager = await contract.methods.manager().call();
+		assert.equal(manager, deployer);
 	});
 });
