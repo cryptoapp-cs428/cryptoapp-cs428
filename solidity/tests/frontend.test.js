0da1a0c435cca93c3037d8553059a2cf93ce3831
Dan Mercer <danielmercer1@gmail.com>
2018-03-23 22:12:46 -0600

    Solidity: added unit test for frontendAPI.personalSign

--- solidity/tests/frontend.test.js
+++ solidity/tests/frontend.test.js
@@ -8,11 +8,11 @@ const Main = require('../build/CryptoShapeMain_full.json');
 const abi = Main['interface'];
 const bytecode = Main['bytecode'];
 
-var mainContract, frontendAPI;
+var accts, mainContract, frontendAPI;
 
 beforeEach(async () => {
 		// Get list of accts
-		var accts = await web3.eth.getAccounts();
+		accts = await web3.eth.getAccounts();
 
 		var sendOpts = {
 			from: accts[0],
@@ -37,6 +37,15 @@ describe("Frontend Solidity API", () => {
 	it("should return the number of shapes in the blockchain", async () => {
 		const count = await frontendAPI.getShapeCount();
 		assert.equal(count, 2);
+	});
+
+	describe("function: personalSign", () => {
+		it("should... do something...", done => {
+			frontendAPI.personalSign("Potato", accts[1], function(err, data) {
+				// personal_sign isn't actually supported by ganace, so this is the best we can do.
+				done();
+			})
+		});
 	});
 });
 
