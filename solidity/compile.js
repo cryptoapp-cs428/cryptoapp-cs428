cf411948f439d68f49180a3f875f0cfbc565becd
Dan Mercer <danielmercer1@gmail.com>
2018-03-23 20:50:31 -0600

    Solidity: Improved compile.js to only recompile if needed

--- solidity/compile.js
+++ solidity/compile.js
@@ -3,30 +3,37 @@ const fs = require('fs-extra');
 const solc = require('solc');
 
 const buildPath = path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);
 const abisPath = path.resolve(__dirname, 'facades', 'build_abis');
-fs.removeSync(abisPath);
+
+const lastBuildTime = getLastBuildTime(buildPath, abisPath);
+console.log("Last Build Time:", new Date(lastBuildTime));
+var buildDirsHaveBeenDeleted = false;
 
 const contractsPath = path.resolve(__dirname, 'contracts');
 const files = fs.readdirSync(contractsPath);
 
 var contracts = {};
 for (let file of files) {
-	try {
+	const filePath = path.resolve(contractsPath, file);
+	if (fs.statSync(filePath).mtime > lastBuildTime) {
+		if (!buildDirsHaveBeenDeleted) removeDirs(buildPath, abisPath);
+
 		console.log(`Compiling ${file} ...`);
 		const filePath = path.resolve(contractsPath, file);
 		const src = fs.readFileSync(filePath, 'utf8');
 		const output = solc.compile(src, 1);
+
 		if (output.errors.length) {
 			output.errors.forEach(err => console.error(err));
 			if (!Object.keys(output.contracts).length) {
-				console.error("No contracts were successfully compiled");
+				console.error(`Could not compile ${file}`);
 				process.exit(1);
 			}
 		}
+
 		Object.assign(contracts, output.contracts);
-	} catch (err) {
-		console.error(`Could not compile ${file}`, err);
+	} else {
+		console.log(`No changes made to ${file} since last build`);
 	}
 }
 
@@ -47,4 +54,16 @@ for (let name in contracts) {
 			{ spaces: '\t' }
 		);
 	}
+}
+
+function getLastBuildTime(...paths) {
+	return paths
+			.filter(path => fs.existsSync(path))
+			.map(path => fs.statSync(path).mtime.getTime())
+			.reduce((a, b) => Math.min(a, b));
+}
+
+function removeDirs(...paths) {
+	paths.filter(path => fs.existsSync(path))
+			.forEach(path => fs.removeSync(path));
 }
