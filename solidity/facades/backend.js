e6b64962abeeb5fbc0a0f7b4e569595987e7b096
Dan Mercer <danielmercer1@gmail.com>
2018-03-28 16:07:56 -0600

    Solidity: partial fix of backend api events
    The syntax being used didn't have the correct function signature for
    the callback. I switched it to the other style and it's sort of
    working.

--- solidity/facades/backend.js
+++ solidity/facades/backend.js
@@ -6,26 +6,14 @@ const Shape = require('./shape');
 useWeb3(require('../web3/rinkeby'), require('../web3/rinkeby-ws'));
 
 var web3, wsWeb3;
-var mainContract;
-var eventShapeAdded, eventChallengePosted, eventChallengeResolved, eventChallengeRejected, eventRandomPosted, eventRandomResolved;
+var mainContract, wsMainContract;
 
 function useWeb3(newWeb3, newWsWeb3, addressOverride) {
 	Shape.useWeb3(newWeb3);
 	web3 = newWeb3;
 	wsWeb3 = newWsWeb3;
 	mainContract = new web3.eth.Contract(mainABI, addressOverride || address);
-	// mainContract = MainContract.at(address);
-	getEvents();
-}
-
-function getEvents() {
-	const wsMainContract = new wsWeb3.eth.Contract(mainABI, address);
-	eventShapeAdded = wsMainContract.events.ShapeAdded({}, {fromBlock: 0, toBlock: 'latest'});
-	eventChallengePosted = wsMainContract.events.ChallengePosted({}, {fromBlock: 0, toBlock: 'latest'});
-	eventChallengeResolved = wsMainContract.events.ChallengeResolved({}, {fromBlock: 0, toBlock: 'latest'});
-	eventChallengeRejected = wsMainContract.events.ChallengeRejected({}, {fromBlock: 0, toBlock: 'latest'});
-	eventRandomPosted = wsMainContract.events.RandomPosted({}, {fromBlock: 0, toBlock: 'latest'});
-	eventRandomResolved = wsMainContract.events.RandomResolved({}, {fromBlock: 0, toBlock: 'latest'});
+	wsMainContract = new wsWeb3.eth.Contract(mainABI, addressOverride || address);
 }
 
 /* on(eventKey, callback) Example usage:
@@ -43,42 +31,43 @@ function getEvents() {
 function on(eventKey, callback) {
 	switch (eventKey) {
 		case "shapeAdded":
-			eventShapeAdded.on('data', function(error, result) {
+			wsMainContract.events.ShapeAdded({ fromBlock: 0 }, function(error, result) {
+				console.log("called!", error, result);
 				if (!error) {
 					callback(result.args.shapeAddress, result.args.owner);
 				}
 			});
 			break;
 		case "challengePosted":
-			eventChallengePosted.on('data', function(error, result) {
+			wsMainContract.events.ChallengePosted({ fromBlock: 0 }, function(error, result) {
 				if (!error) {
 					callback(result.args.sourceShape, result.args.targetShape);
 				}
 			});
 			break;
 		case "challengeResolved":
-			eventChallengeResolved.on('data', function(error, result) {
+			wsMainContract.events.ChallengeResolved({ fromBlock: 0 }, function(error, result) {
 				if (!error) {
 					callback(result.args.sourceShape, result.args.targetShape, result.args.sourceWon);
 				}
 			});
 			break;
 		case "challengeRejected":
-			eventChallengeRejected.on('data', function(error, result) {
+			wsMainContract.events.ChallengeRejected({ fromBlock: 0 }, function(error, result) {
 				if (!error) {
 					callback(result.args.sourceShape, result.args.targetShape);
 				}
 			});
 			break;
 		case "randomPosted":
-			eventRandomPosted.on('data', function(error, result) {
+			wsMainContract.events.RandomPosted({ fromBlock: 0 }, function(error, result) {
 				if (!error) {
 					callback(result.args.shapeAddress);
 				}
 			});
 			break;
 		case "randomResolved":
-			eventRandomResolved.on('data', function(error, result) {
+			wsMainContract.events.RandomResolved({ fromBlock: 0 }, function(error, result) {
 				if (!error) {
 					callback(result.args.winnerShapeAddress, result.args.loserShapeAddress);
 				}
