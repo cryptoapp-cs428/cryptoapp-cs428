1743ca80cb62d6abe6f11ba00cdb10253501bbe4
Dan Mercer <danielmercer1@gmail.com>
2018-03-30 12:31:20 -0600

    Revert "Events unit tests: got main contract deployment working with truffle-contract on ganache."
    This reverts commit b787273cd37bf2b29e6303d07655ad2085265d85.

--- solidity/tests/events.test.js
+++ solidity/tests/events.test.js
@@ -1,9 +1,6 @@
 require('../scripts/compile');
 const assert = require('assert');
 const backendFacade = require('../facades/backend');
-
-const truffleContract = require('truffle-contract');
-
 const web3 = require('../web3/ganache');
 const wsWeb3 = require('../web3/ganache');
 
@@ -21,44 +18,32 @@ before(async function() {
 	accts = await web3.eth.getAccounts();
 	deployer = accts[0];
 	user1 = accts[1];
-	console.log("Accounts obtained");
-
 	// Use acct to deploy contract
-	const provider = web3.currentProvider;
-	assert.ok(provider, "web3.currentProvider is not ok");
-	mainContract = await deployContract(JSON.parse(abi), bytecode, provider, deployer);
-	assert.ok(mainContract);
-	console.log("Main contract deployed");
+	mainContract = await new web3.eth.Contract(JSON.parse(abi))
+		.deploy({
+			data: bytecode
+		})
+		.send({
+			from: deployer,
+			gas: '6000000'
+		});
+	assert.ok(mainContract.options.address);
 
-	backendFacade.useWeb3(web3, wsWeb3, mainContract.address);
-	console.log("backendFacade set up");
+	backendFacade.useWeb3(web3, wsWeb3, mainContract.options.address);
 });
 
 //======================================================================
 //			Utility functions
 
-function deployContract(abi, bytecode, provider, deployFrom) {
-	var MainContract = truffleContract({
-		abi: abi,
-		unlinked_binary: bytecode,
-	});
-	MainContract.setProvider(provider);
-	console.log("Deploying from ", deployFrom);
-	return MainContract.new({
-		from: deployFrom,
-		gas: '6000000',
-	});
-}
-
 function deployShapeFrom(acct) {
-	const prom = mainContract.buyShape({
+	const prom = mainContract.methods.buyShape().send({
 		from: acct,
 		value: web3.utils.toWei('0.01', 'ether'),
 		gas: '6000000'
 	});
 	// Return utility to get address of shape
 	prom.andGetAddress = () => prom
-		.then(() => mainContract.getShapes({
+		.then(() => mainContract.methods.getShapes().call({
 			from: acct
 		}))
 		.then(shapes => shapes[shapes.length - 1]);
